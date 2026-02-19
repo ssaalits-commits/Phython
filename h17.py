20d10c3cb694b0849bbb7a866b6a2bad8400185b
ssaalits-commits <ssaalits@hkhk.edu.ee>
2026-02-19 09:51:53 +0200

    sd

--- h17.py
+++ h17.py
@@ -1,9 +1,23 @@
+import os
 # Pangakonto – pangakonto.txt
 # Sinu ülesandeks on kirjutada Pythoni skript, mis loeb andmeid failist pangakonto.txt. Fail sisaldab eraldi ridadel pangatehingute summasid: positiivsed summad tähistavad sissetulekuid ja negatiivsed summad väljaminekuid. Skript peab arvutama ja väljastama:
 # kogu tehingute arvu
 # positiivsete tehingute arvu
 # positiivsete tehingute kogusumma
 # Tulemused tuleb väljastada konsooli
+
+tehingute_arv = 0
+pos_tehingute_arv = 0
+pos_tehingute_sum = 0
+
+
 with open("pangakonto.txt") as fail:
     for rida in fail:
-        print(rida.strip()) 
+        tehingute_arv+= 1
+        if float(rida.strip()) > 0:
+            pos_tehingute_arv+= 1
+            pos_tehingute_sum+=float(rida.strip())
+            print(rida.strip())
+print(f"tehingute arv: {tehingute_arv}")
+print(f"pos. tehingute arv: {pos_tehingute_arv}")
+print(f"Pos. tehingute summa: {pos_tehingute_sum: .2f}")
---
+++ h18.py
@@ -0,0 +1,31 @@
+import csv
+# Korvpalli tulemused: EstonianBasketballGames.csv
+# Lae andmed alla ja lahenda järgmised probleemid (iga punkt eraldi lahendus):
+# Kuva mitu meeskonda osales ja too välja nimekiri (täpitähed peavad korras olema ja jälgi, et esimese rea jätad vahele)
+# Leia mitu mängu igal meeskonnal oli (kasuta sõnastikku, kui meeskond on juba sõnastikus, siis suurendad selle väärtust +1)
+# Leia iga meeskonna võidud ja kaotused ning kirjuta tulemused uude Exceli sõbralikku CSV faili_nimi = 'users.csv'
+
+
+
+
+
+faili_nimi= 'Phython it25/eesti_korka_mängud.csv'
+
+meeskonnad = []
+
+
+with open(faili_nimi, mode='r', encoding='utf-8') as fail:
+   
+    csv_lugeja = csv.reader(fail)
+    pais = next(csv_lugeja)
+   
+    print(f"Päise veerud: {pais}")
+    for rida in csv_lugeja:
+        if rida[1] not in meeskonnad:
+            meeskonnad.append(rida[1])
+        if rida[2] not in meeskonnad:
+            meeskonnad.append(rida[2])
+        #print(rida[1])
+
+
+print(f"Meeskonnad kokku: {len(meeskonnad)}")
