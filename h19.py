0eb00ee91b83432c91e674b7ea700b5d463de2bf
ssaalits-commits <ssaalits@hkhk.edu.ee>
2026-03-06 09:30:53 +0200

    asdas

--- h19.py
+++ h19.py
@@ -5,12 +5,32 @@ import json
 # Kuva millistes trennides 12. klassi õpilased käivad
 # Kuva 12 klassi õpilaste hinneteleht (nimi, ained, punktid)
 
-with open('failid/tulemused.json', 'r', encoding='utf-8') as file:
+with open('haridustulemused.json', 'r', encoding='utf-8') as file:
     tulemused = json.load(file)
 
-
+klass12=0
+klass11=0
+klass10=0
+tegevused=[]
 
 for tulem in tulemused:
     #print(tulem['klass'])
     if tulem['klass'] == "12":
         print(tulem['nimi'])
+        for aine, hinne in tulem['hinded'].items():
+            print("\t", aine, hinne)
+        klass12 +=1
+        for tegevus in (tulem['tegevused']):
+            if tegevus not in tegevused: 
+                tegevused.append(tegevus)
+    elif tulem['klass'] =="11":
+        klass11 +=1
+    else:
+        klass10 +=1
+
+print(f"12.klassis {klass12} õpilast")
+print(f"11.klassis {klass11} õpilast")
+print(f"10.klassis {klass10} õpilast")
+for t in tegevused:
+    print(t, end=", ")
+        
