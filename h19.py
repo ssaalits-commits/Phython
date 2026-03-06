3c54df126beb08757e0352a578cd576202c2f7c3
ssaalits-commits <ssaalits@hkhk.edu.ee>
2026-03-06 08:58:23 +0200

    dsa

--- h19.py
+++ h19.py
@@ -1,9 +1,16 @@
 import json
 
+# Kuva 12.klassi õpilaste nimekiri
+# Kuva mitu õpilast õpib 10, 11 ja 12 klassis
+# Kuva millistes trennides 12. klassi õpilased käivad
+# Kuva 12 klassi õpilaste hinneteleht (nimi, ained, punktid)
 
-with open('failid/books.json', 'r', encoding='utf-8') as file:
-    jason = json.load(file)
+with open('failid/tulemused.json', 'r', encoding='utf-8') as file:
+    tulemused = json.load(file)
+
 
 
 for tulem in tulemused:
-    print(tulem)
+    #print(tulem['klass'])
+    if tulem['klass'] == "12":
+        print(tulem['nimi'])
