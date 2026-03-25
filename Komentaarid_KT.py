b161d7aefae023e7371d270555d357149341a6ed
ssaalits-commits <ssaalits@hkhk.edu.ee>
2026-03-25 13:50:30 +0200

    <zxc<zxc<

--- Komentaarid_KT.py
+++ Komentaarid_KT.py
@@ -1,10 +1,20 @@
-import jason
+import json
 import requests
 
-
-url= "https://dummyjson.com/comments"
-
-
+url = "https://dummyjson.com/comments"
 response = requests.get(url)
+comments = response.json()
+nimi = "jaces"
 
-# if response.status_code == 200:
+for comment in comments['comments']:
+   
+    if nimi == comment['user']['username']:
+         print(comment['body'])
+
+komentaarid = len(comments['comments'])
+print(f"Koguarv kommentaare: {komentaarid}")
+
+rohkem_meeldinud_komentaarid = max(comments['comments'], key=lambda x: x['likes'])
+print(f"Enim meeldimisi saanud kommentaar: {rohkem_meeldinud_komentaarid['body']} - Meeldimised: {rohkem_meeldinud_komentaarid['likes']}")
+
+
